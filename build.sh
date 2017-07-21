9019748a3398a0ea72e18cc9c82798771a2b147d
smsohan <sohan39@gmail.com>
2017-07-21 12:34:54 -0600

    remove unwanted files

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pdflatex controlled_study.tex
-bibtex controlled_study.aux
-pdflatex controlled_study.tex
-pdflatex controlled_study.tex
+pdflatex root.tex
+bibtex root.aux
+pdflatex root.tex
+pdflatex root.tex
