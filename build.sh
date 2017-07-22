d49a6d1e44adace031acc3db7b09f64067098be8
smsohan <sohan39@gmail.com>
2017-07-22 01:32:03 -0600

    adds the case study paper

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ set -e
 git clean -fX
 pdflatex root.tex
 bibtex controlled_study/controlled_study.aux
+bibtex case_study/case_study.aux
 pdflatex root.tex
 pdflatex root.tex
 /Applications/Skim.app/Contents/MacOS/Skim root.pdf
