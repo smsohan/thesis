95e2383b30b98205b9e385314f19cdf59ee62ab2
smsohan <sohan39@gmail.com>
2017-07-22 02:19:59 -0600

    adds the cisco study paper

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ git clean -fX
 pdflatex root.tex
 bibtex controlled_study/controlled_study.aux
 bibtex case_study/case_study.aux
+bibtex cisco_study/cisco_study.aux
 pdflatex root.tex
 pdflatex root.tex
 /Applications/Skim.app/Contents/MacOS/Skim root.pdf
