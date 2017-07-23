568d84f106570c7bfde0354b7142db94a6af2dac
smsohan <sohan39@gmail.com>
2017-07-23 16:58:08 -0600

    all chapters added

--- build.sh
+++ build.sh
@@ -8,4 +8,17 @@ bibtex case_study/case_study.aux
 bibtex cisco_study/cisco_study.aux
 pdflatex root.tex
 pdflatex root.tex
-/Applications/Skim.app/Contents/MacOS/Skim root.pdf
+
+
+pdffile=root.pdf
+/usr/bin/osascript << EOF
+  set theFile to POSIX file "${pdffile}" as alias
+  tell application "Skim"
+  # uncomment the folloing line if you want Skim to get the focus
+  activate
+  set theDocs to get documents whose path is (get POSIX path of theFile)
+  if (count of theDocs) > 0 then revert theDocs
+  open theFile
+  end tell
+EOF
+
